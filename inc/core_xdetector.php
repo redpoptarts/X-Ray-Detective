3f8a1043d766bda6bc3f72f2a90ebdbb07a23d7d
redpoptarts <redpoptarts@gmail.com>
2012-04-08 07:12:33 -0500

    Increase version to v0.03.00a, Update changelog.txt

--- inc/core_xdetector.php
+++ inc/core_xdetector.php
@@ -11,7 +11,7 @@ require_once( dirname(__FILE__) . '/core_config_handler.php');
 //
 // IRC Channel: (irc.esper.net) #xray
 //
-// Version: v0.02.02a
+// Version: v0.03.00a
 //
 //=====================================================
 //
