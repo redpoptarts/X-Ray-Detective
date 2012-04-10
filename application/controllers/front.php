3ef18b4dfad9082eac845c8e6ea4c21296477370
Geoff Winans <gwinans@gmail.com>
2012-04-09 21:38:03 -0700

    Skeleton methods added: update_stats, clear_xrd_db, settings

--- application/controllers/front.php
+++ application/controllers/front.php
@@ -12,7 +12,26 @@ class Front extends CI_Controller {
 		
 		$this->load->model('config_model', 'config', TRUE);
 		$this->load->model('logger_model', 'logger', TRUE);
+	}
 
+	public function index()
+	{
+		$this->view->load('front');
+	}
+
+	public function update_stats()
+	{
+		// Here, we update the stats.
+	}
+
+	public function clear_xrd_db()
+	{
+		// Here, we clear the DB for new stats
+	}
+
+	public function settings()
+	{
+		// Here, we change the XRD configuration.
 	}
 
 }
